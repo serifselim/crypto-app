9646503d18f75089b69f94469cbe6a58a3212dd5
Yavuz Selim Şerifoğlu <70685127+serifselim@users.noreply.github.com>
2022-05-01 04:26:40 +0300

    create files

---
+++ src/globalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+const globalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html{
+        font-size: 10px;
+    }
+
+    body{
+
+    }
+`;
+
+export default globalStyle;
